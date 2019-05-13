d65422185f096f6ba6dfc7e93f253d836ca969df
Araceli Miyanomori <Araceli Miyanomori@LAPTOP-TV3PHC9Q>
2019-05-13 11:53:30 -0700

    Agregar archivos de proyecto.

---
+++ PlantillaOpenGL/Carro.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Modelo.h"
+#include "glm/gtx/transform.hpp"
+#include "glm/glm.hpp"
+
+class Carro : public Modelo {
+public:
+	Carro();
+	void avanzar();
+	void rotar();
+	vec3 getCoordenadas();
+private: 
+	vec3 coordenadas = vec3(0.0f, 0.0f, 0.0f);
+	void actualizarMatrizModelo();
+};
